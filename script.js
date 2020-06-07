16780947d99d5d4baf63450b75f7637ca566a98a
E Swarup Kumar <44138255+eswarupkumar@users.noreply.github.com>
2020-06-07 12:35:00 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var compscore=0
-var userscore=0
+var compscore = 0
+var userscore = 0
+
+var noofmatch = document.querySelector("pmatch")
 function play(clicked_id) {
-    var compscore_span=document.querySelector("#comp-score")
-    var userscore_span=document.querySelector("#user-score")
+    noofmatch++
+    document.getElementById("pmatch").innerHTML = noofmatch
+
+    var compscore_span = document.querySelector("#comp-score")
+    var userscore_span = document.querySelector("#user-score")
 
     var img = document.getElementById(clicked_id)
     var clone = img.cloneNode(true)
-    document.getElementById("player").innerHTML=""
-    document.getElementById("computer").innerHTML=""
+    document.getElementById("player").innerHTML = ""
+    document.getElementById("computer").innerHTML = ""
     document.getElementById("player").appendChild(clone)
 
     var reflect
@@ -48,38 +53,70 @@ function play(clicked_id) {
     else if (num == 0) {
         if (reflect == 1) {
             compscore++
-            compscore_span.innerHTML=compscore
+            compscore_span.innerHTML = compscore
             document.getElementById("Result").innerHTML = "YOU LOST 💩"
         }
         else {
             userscore++
-            userscore_span.innerHTML=userscore
+            userscore_span.innerHTML = userscore
             document.getElementById("Result").innerHTML = "YOU WON 😁"
         }
     }
     else if (num == 1) {
         if (reflect == 0) {
             userscore++
-            userscore_span.innerHTML=userscore
+            userscore_span.innerHTML = userscore
             document.getElementById("Result").innerHTML = "YOU WON 😁"
         }
         else {
             compscore++
-            compscore_span.innerHTML=compscore
+            compscore_span.innerHTML = compscore
             document.getElementById("Result").innerHTML = "YOU LOST 💩"
         }
     }
     else if (num == 2) {
         if (reflect == 0) {
             compscore++
-            compscore_span.innerHTML=compscore
+            compscore_span.innerHTML = compscore
             document.getElementById("Result").innerHTML = "YOU LOST 💩"
         }
         else {
             userscore++
-            userscore_span.innerHTML=userscore
+            userscore_span.innerHTML = userscore
             document.getElementById("Result").innerHTML = "YOU WON 😁"
         }
     }
-}
+    if (noofmatch == 5) {
+        noofmatch = 0
+        
+        var modal = document.getElementById("myModal");
+        var span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+        if(compscore==userscore){
+            document.getElementById("text").innerHTML="It's a Tie🤝 but what a nail bitting match 😮!!"
+        }
+        else if(compscore>userscore){
+            document.getElementById("text").innerHTML="Computer owned this match 🤖. Call for a next match 💪? "
+        }
+        else if(compscore<userscore){
+            document.getElementById("text").innerHTML="Wohooo 🤩!! Belived in you 😇 "
+        }
+        
 
+
+        span.onclick = function() {
+            modal.style.display = "none";
+            setTimeout(reload,0)
+          }
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+              setTimeout(reload,0)
+            }
+          }
+        
+    }
+}
+function reload(){
+    location.reload()
+}
